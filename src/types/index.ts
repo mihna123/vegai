155f50f76803ceb41fc5d178aaf75a5123879001
mihailo.vojinovic <mihailonvojinovic@gmail.com>
2025-09-14 16:26:58 +0200

    Add id to user type

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,11 @@
+import { ObjectId } from "mongodb";
+
 export type User = {
+  _id: ObjectId;
   email: string;
   role: "advisor" | "introducer" | "manager" | "esmanager";
 };
+
+interface UserClient extends Omit<User, "_id"> {
+  _id: string;
+}
